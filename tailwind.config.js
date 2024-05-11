121259b2ac8bae2b125dae28ab72dcda582bfee3
ardigiandi <ardiansyahg9080@gmail.com>
2024-05-11 23:30:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'abu' : '#F7F6F4',
+        'oren': '#ED553B',
+      }
+    },
+  },
+  plugins: [],
+}
+
